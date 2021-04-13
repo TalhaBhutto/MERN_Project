1cfac6681f27ab6ae9f476588ab6e5cb226271a1
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-14 02:49:01 +0500

    basic structure of App Post/Form

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Post from './Post/Post'
+
+function Posts() {
+    return (
+        <div>
+            <h1>Posts</h1>
+        </div>
+    )
+}
+
+export default Posts
