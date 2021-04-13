20d5dc0516ef24122edb1023b443b3ce2579990b
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-14 00:10:10 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,11 @@ import bodyparser from 'body-parser';
 import mongooses from 'mongoose';
 import cors from 'cors';
 
+//set up express and added body paret to limit the image size
 const app=express()
 app.use(bodyparser.json({limit:"30mb",extended:true}));
 app.use(bodyparser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
+
+//mongoDB connection link
+const CONNECTION_URL = 'mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
