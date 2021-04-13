b30b306ab62f76fae5d4d563702ea61ad226f453
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-14 01:26:05 +0500

    almost implemented the create post functionality

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,10 @@
 // All the routes related to POST will be here
 import express from 'express';
+import {getPosts,createPost} from '../controllers/posts.js'
 
 const router=express.Router();
 
-router.get('/',(req,res)=>{
-    res.send('This WORKS!');
-});
+router.get('/',getPosts);
+router.post('/',createPost);
 
 export default router;
