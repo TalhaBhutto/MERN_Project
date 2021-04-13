0fd016a5c942d7762e8d7911aa1d7acb35e7394d
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-13 23:37:23 +0500

    imported necessary packages in index.js on the server side

--- server/index.js
+++ server/index.js
@@ -1,2 +1,5 @@
 // added "type":"module", in package.json to use the react type syntax of importing files
-import express from 
+import express from 'express';
+import bodyparser from 'body-parser';
+import mongooses from 'mongoose';
+import cors from 'cors';
