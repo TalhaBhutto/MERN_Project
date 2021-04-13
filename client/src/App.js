7d16659334a13a74b5a05111bd5376a957a4b9d0
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-14 01:51:00 +0500

    made the folder structure of the client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
 
 const App = () =>{
     return (
-        <div>
-            <h1>App</h1>
-        </div>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit"/>
+        </Container>
     );
 }
 
