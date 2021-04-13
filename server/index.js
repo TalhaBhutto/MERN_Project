46a6e670977c74a0c220cb40d4d9ec4a321c6279
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-13 23:58:01 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -5,5 +5,6 @@ import mongooses from 'mongoose';
 import cors from 'cors';
 
 const app=express()
-app.use(bodyparser.json({limit:"30mb",extended:true}))
-app.use(bodyparser.urlencoded({limit:"30mb",extended:true}))
+app.use(bodyparser.json({limit:"30mb",extended:true}));
+app.use(bodyparser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors());
