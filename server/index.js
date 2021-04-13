c2ad194ddbe534dd6d158e8e67009b784dfbaccd
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-14 00:13:53 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // added "type":"module", in package.json to use the react type syntax of importing files
 import express from 'express';
 import bodyparser from 'body-parser';
-import mongooses from 'mongoose';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 //set up express and added body paret to limit the image size
@@ -12,3 +12,8 @@ app.use(cors());
 
 //mongoDB connection link
 const CONNECTION_URL = 'mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// port address =>> for local host:5000 =>> for heroku process.env.PORT
+const PORT=process.env.PORT||5000;
+
+//connecting with mongoDB =>> useNewUrlParser:true,useUnifiedTopology:true are not required but we set them true to avoid errors in console
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true});
