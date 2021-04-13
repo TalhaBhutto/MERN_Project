30af50bdc4d70f4eb3160fb614f64ec734af0f91
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-14 02:54:38 +0500

    front end structure modified

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -3,9 +3,11 @@ import Post from './Post/Post'
 
 function Posts() {
     return (
-        <div>
-            <h1>Posts</h1>
-        </div>
+        <>
+            <h1>POSTS</h1>
+            <Post/>
+            <Post/>
+        </>
     )
 }
 
