ca80b61b116bbc7fe61305b9125cfb615bde356c
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-14 14:08:16 +0500

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,3 +1,10 @@
-const reducer =(state,action)=>{
-    
+const reducer =(posts=[],action)=>{
+    switch(action.type){
+        case 'FETCH_ALL':
+            return posts;
+        case 'CREATE':
+            return posts;
+        default:
+            return posts;
+    }
 }
