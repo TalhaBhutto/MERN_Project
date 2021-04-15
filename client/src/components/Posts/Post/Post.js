e8dd5d49bfb69455367f10694df010df7b0cfcc3
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-15 23:15:09 +0500

    Update Post.js

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import useStyles from './styles';
+import {Card,CardActions,CardContent,CardMedia,Button,Typography} from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
 
-function Post() {
+function Post({post}) {
     const classes=useStyles();
     return (
-        <div>
-            <h1>Post</h1>
-        </div>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+            <div className={classes.overlay}>
+                <Typography varient="h6">{post.creator}</Typography>
+                <Typography varient="h6">{post.creator}</Typography>
+            </div>
+        </Card>
     )
 }
 
