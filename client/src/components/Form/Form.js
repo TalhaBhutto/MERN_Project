871b06e5a0fd457b6e4557412d80a664e06e1e52
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-15 18:44:14 +0500

    upload image

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -29,7 +29,8 @@ function Form() {
                 <TextField name="message" variant="outlined" label="message" fullWidth value={postData.message} onChange={(e)=>setPostData({...postData,message:e.target.value})}/>
                 <TextField name="tags" variant="outlined" label="tags" fullWidth value={postData.tags} onChange={(e)=>setPostData({...postData,tags:e.target.value})}/>
                 <div className={classes.fileInput}>
-                    <FileBase type="file" multiple={false} onDone={(base64)=>setPostData({...postData,selectedFile:base64})}/>
+                    {/* <FileBase type="file" multiple={false} onDone={(base64)=>setPostData({...postData,selectedFile:base64})}/> */}
+                    <input type="file" multiple={false} onDone={(base64)=>setPostData({...postData,selectedFile:base64})}/>
                 </div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
