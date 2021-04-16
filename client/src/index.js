d723dc77264bef97f1ce95e431f47b1cf3f26669
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-16 23:33:47 +0500

    Mern

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import {Provider} from 'react-redux';
 import {createStore,applyMiddleware,compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers'
+import './index.css';
 
 const store=createStore(reducers,compose(applyMiddleware(thunk)));
 
