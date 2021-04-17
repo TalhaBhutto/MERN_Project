49c9f78a7c91c1c1418d15ddf47e0c8ad3cc66d0
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-17 23:45:52 +0500

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -25,3 +25,15 @@ export const createPost=async (req,res)=>{
 
     }
 }
+export const createPost=async (req,res)=>{
+    const post =req.body;
+    const newPost=new PostMessage(post);
+    try{
+        await newPost.save();
+        res.status(201).json(newPost);
+    }
+    catch(error){
+        res.status(409).json({message:error.message});
+
+    }
+}
