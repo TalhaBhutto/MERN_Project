9cc9f4f78286c2f67de63c5f2778588dc6e2a839
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-18 00:40:32 +0500

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,6 +1,6 @@
 // All the routes related to POST will be here
 import express from 'express';
-import {getPosts,createPost} from '../controllers/posts.js'
+import {getPosts,createPost,updatePost} from '../controllers/posts.js'
 
 const router=express.Router();
 
