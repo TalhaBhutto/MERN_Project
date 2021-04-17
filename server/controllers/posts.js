64846cf682daad5ce60a141ee9124e09c31d5149
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-17 23:51:38 +0500

    Update posts.js

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -28,6 +28,7 @@ export const createPost=async (req,res)=>{
 }
 export const updatePost=async (req,res)=>{
     const {id:_id}=req.params;
+    const post=req.body;
     if(!Mongoose.prototype.ObjectId.isValid(_id)) return res.status(404).send("No post with that id.");
-    PostMessage.findByIdAndUpdate(_id,)
+    const updatedPost=await PostMessage.findByIdAndUpdate(_id,post,{new:true});
 }
