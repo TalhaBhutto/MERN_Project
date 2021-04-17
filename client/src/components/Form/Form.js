f2bebfea0d9f1bd7b061d90794edf0427e5691f8
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-17 23:56:38 +0500

    Update Form.js

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -8,7 +8,7 @@ import { createPost } from '../../actions/posts';
 
 function Form() {
     const [postData, setPostData] = useState({
-        creator:'',title:'',message:'',tags:'',selectedFile:''
+        creator:'',title:'',message1:'',tags:'',selectedFile:''
     });
     const classes=useStyles();
     const dispatch = useDispatch();
