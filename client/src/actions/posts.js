852ea6862a434497dcb42a8f65992faaa096ca27
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-18 00:37:16 +0500

    Update posts.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -19,7 +19,7 @@ export const createPost=(post)=> async (dispatch)=>{
         console.log(error.message);
     }
 }
-export const createPost=(id,post)=> async (dispatch)=>{
+export const updatePost=(id,post)=> async (dispatch)=>{
     try{
         const {data}=await api.updatePost(id,post);
         dispatch({type:'UPDATE',payload:data});
