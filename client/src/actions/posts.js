6d34d0109f7124972470f9519c39b44839cf0ea0
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-18 22:01:18 +0500

    Update posts.js

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,10 +1,10 @@
 import * as api from '../Api';
-import {CREATE,UPDATE,DELETE,FETCH_ALL,LIKE} from '../constants/actionTypes';
+import {CREATE,UPDATE,DELETE,FETCH_ALL} from '../constants/actionTypes';
 
 export const getPosts=()=> async (dispatch)=>{
     try{
         const {data}=await api.fetchPosts();
-        dispatch({type:'FETCH_ALL',payload:data});
+        dispatch({type:FETCH_ALL,payload:data});
     }
     catch(error){
         console.log(error.message);
@@ -14,7 +14,7 @@ export const getPosts=()=> async (dispatch)=>{
 export const createPost=(post)=> async (dispatch)=>{
     try{
         const {data}=await api.createPost(post);
-        dispatch({type:'CREATE',payload:data});
+        dispatch({type:CREATE,payload:data});
     }
     catch(error){
         console.log(error.message);
@@ -23,7 +23,7 @@ export const createPost=(post)=> async (dispatch)=>{
 export const updatePost=(id,post)=> async (dispatch)=>{
     try{
         const {data}=await api.updatePost(id,post);
-        dispatch({type:'UPDATE',payload:data});
+        dispatch({type:UPDATE,payload:data});
     }
     catch(error){
         console.log(error.message);
@@ -33,7 +33,7 @@ export const updatePost=(id,post)=> async (dispatch)=>{
 export const deletePost=(id)=> async (dispatch)=>{
     try{
         await api.deletePost(id);
-        dispatch({type:'DELETE',payload:id});
+        dispatch({type:DELETE,payload:id});
     }
     catch(error){
         console.log(error.message);
@@ -42,7 +42,7 @@ export const deletePost=(id)=> async (dispatch)=>{
 export const likePost=(id)=> async (dispatch)=>{
     try{
         const {data}=await api.likePost(id);
-        dispatch({type:'UPDATE',payload:data});
+        dispatch({type:UPDATE,payload:data});
     }
     catch(error){
         console.log(error);
