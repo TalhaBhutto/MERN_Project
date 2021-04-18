2ab00cc87b37f5eda17f9590534025b3d30e927b
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-18 22:11:22 +0500

    added dotEnv

--- server/index.js
+++ server/index.js
@@ -15,13 +15,13 @@ app.use(bodyparser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 app.use('/posts',postRoutes);
 //mongoDB connection link
-const CONNECTION_URL = 'mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/memories?retryWrites=true&w=majority';
+//const CONNECTION_URL = 'mongodb+srv://Talha:lYDxLcpsmAs2gC0Y@cluster0.gmnqm.mongodb.net/memories?retryWrites=true&w=majority';
 // port address =>> for local host:5000 =>> for heroku process.env.PORT
 const PORT=process.env.PORT||5000;
 
 //connecting with mongoDB =>> useNewUrlParser:true,useUnifiedTopology:true are not required but we set them true to avoid errors in console
 //=>> .then run the app =>> .catch print the error
-mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=> app.listen(PORT,()=>console.log(`Server running on ${PORT}`)))
 .catch((error)=>console.log(error.message))
 
