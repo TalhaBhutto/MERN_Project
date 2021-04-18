8b78ce44a9199d99a87ae257ca2e2dd1c0416f97
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-18 19:49:10 +0500

    LIKE POST WORKING!!

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -41,7 +41,7 @@ export const deletePost=(id)=> async (dispatch)=>{
 export const likePost=(id)=> async (dispatch)=>{
     try{
         const {data}=await api.likePost(id);
-        dispatch({type:'LIKE',payload:data});
+        dispatch({type:'UPDATE',payload:data});
     }
     catch(error){
         console.log(error);
