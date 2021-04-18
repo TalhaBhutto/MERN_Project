25565ed893e9ec537c8d87a49672906d557bbe59
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-18 18:14:33 +0500

    Update Post.js

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,8 +5,11 @@ import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
+import {useDispatch} from 'react-redux';
+import {deletePost} from '../../../actions/posts'
 
 const Post=({post,setCurrentId})=> {
+    const dispatch = useDispatch();
     const classes=useStyles();
     const displayTags=(p=post.tags)=>{
         const t=p[0].split(" ");
@@ -37,7 +40,7 @@ const Post=({post,setCurrentId})=> {
                     Like 
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={()=>{}}>
+                <Button size="small" color="primary" onClick={()=>dispatch(deletePost)}>
                     <DeleteIcon fontSize="small"/>
                     Delete
                 </Button>
