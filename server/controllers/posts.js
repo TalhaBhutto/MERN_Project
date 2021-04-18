8df19e9c7e68afde76258e163320c655e7c73046
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-18 14:30:08 +0500

    implementing the delete post logic

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -30,5 +30,8 @@ export const updatePost=async (req,res)=>{
     res.json(updatedPost);
 }
 export const deletePost=async (req,res)=>{
-    
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No post with that id.");
+    await postMessage.findByIdAndDelete(id);
+    res.json({message:'post deleted successfully'});
 }
