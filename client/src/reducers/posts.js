ddc7438067059514c46bf1eeafb668091667e18e
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-18 22:04:28 +0500

    Update posts.js

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,13 +1,14 @@
+
+import {CREATE,UPDATE,DELETE,FETCH_ALL} from '../constants/actionTypes';
 export default (posts=[],action)=>{
     switch(action.type){
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
-        case 'LIKE':
+        case CREATE:
             return [...posts,action.payload];
-        case 'UPDATE':
+        case UPDATE:
             return posts.map((post)=>post._id === action.payload._id?action.payload:post);
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post)=>post._id!==action.payload);
         default:
             return posts;
