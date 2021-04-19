aad4932083fb31fc5bbc050274a8e71ea065a12c
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-20 01:18:02 +0500

    nav bar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {AppBar,Typography} from '@material-ui/core';
 
-export const Navbar = () => {
+const Navbar = () => {
+    const classes=useStyles();
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
             <Typography  className={classes.heading} variant="h2" align="center">Memories</Typography>
@@ -9,3 +10,4 @@ export const Navbar = () => {
             </AppBar>
     )
 }
+export default Navbar;
