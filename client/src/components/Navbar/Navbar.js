81a011ce9dc26b07dabdc585f9b0c9de18a8606a
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-20 17:39:27 +0500

    nav bar almost done

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,7 +3,9 @@ import {Link} from 'react-router-dom';
 import {AppBar,Typography} from '@material-ui/core';
 import memories from '../../images/memories.png';
 import useStyles from './styles';
-import { Link } from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
+import { Avatar } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 
 const Navbar = () => {
     const classes=useStyles();
@@ -13,6 +15,23 @@ const Navbar = () => {
             <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">Memories</Typography>
             <img className={classes.image} src={memories} alt="memories" height="60"/>
             </div>
+            <Toolbar className={classes.toolbar}>
+                {user?(
+                    <div className={classes.profile}>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>
+                            {user.result.name.charAt(0)}
+                            </Avatar>
+                            <Typography className={classes.userName} variant="h6">
+                                {user.result.name}
+                            </Typography>
+                            <Button variant="contained" className={classes.logout} color="secondary">
+                                Logout
+                            </Button>
+                    </div>
+                ):(
+                    <Button component={Link} to="/auth" varient="contained" color="primary">Sign in</Button>
+                )}
+            </Toolbar>
             </AppBar>
     )
 }
