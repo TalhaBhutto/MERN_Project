663378ea6ae3dfa9259c27e6d55bd7a68a90b3d2
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-20 18:19:11 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {Container} from '@material-ui/core';
 import NavBar from './components/Navbar/Navbar';
 import {BrowserRouter,Switch,Route} from 'react-router-dom';
 import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
 
 const App = () =>{
     
@@ -14,7 +15,6 @@ const App = () =>{
                 <Route path="/" exact component={Home}/>
                 <Route path="/auth" exact component={Auth}/>
             </Switch>
-            <Home/>
         </Container>
         </BrowserRouter>
     );
