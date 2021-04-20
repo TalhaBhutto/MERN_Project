e121a9c2a2f39017ca5610df410efb4ef2a7b688
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-20 18:06:33 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import {useEffect,useState} from 'react';
 import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
-import {useDispatch} from 'react-redux';
 import {getPosts} from './actions/posts'
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
 import useStyles from './styles';
 import NavBar from './components/Navbar/Navbar';
 import {BrowserRouter,Switch,ROute} from 'react-router-dom';
+import Home from './components/Home/Home';
 
 const App = () =>{
     
     return (
         <Container maxWidth="lg">
             <NavBar/>
-            
+            <Home/>
         </Container>
     );
 }
