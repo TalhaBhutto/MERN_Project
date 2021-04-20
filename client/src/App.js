93a861cadaa916d931665d3d067c240271bf551a
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-20 18:07:12 +0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {useEffect,useState} from 'react';
 import {Container,AppBar,Typography,Grow,Grid} from '@material-ui/core';
-import {getPosts} from './actions/posts'
 import useStyles from './styles';
 import NavBar from './components/Navbar/Navbar';
 import {BrowserRouter,Switch,ROute} from 'react-router-dom';
