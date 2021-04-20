bc20f3d6e096452d16b87a71b298cca045a51a73
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-20 17:28:16 +0500

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import {AppBar,Typography} from '@material-ui/core';
 import memories from '../../images/memories.png';
 import useStyles from './styles';
