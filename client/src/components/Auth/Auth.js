2dc53054206098bd7635a26a391378994883f8b9
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-21 18:35:05 +0500

    adding auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,18 +1,30 @@
 import React,{useState} from 'react'
-import {Avatar,Button,Paper,Grid,Typography,Container,TextField} from '@material-ui/core';
+import {Avatar,Button,Paper,Grid,Typography,Container} from '@material-ui/core';
+import {GoogleLogin} from 'react-google-login';
 import useStyles from './styles';
 import { LockOutlined } from '@material-ui/icons';
 import Input from './Input';
+import Icon from './icon';
 
 function Auth() {
     const classes=useStyles();
     const [showPassword, setShowPassword] = useState(false);
     const handleShowPassword=()=>setShowPassword((prevShowPassword)=>!prevShowPassword)
-    const isSignup=true;
+    const [isSignup, setisSignup] = useState(false)
     const handleSubmit=()=>{
 
     }
     const handleChange=()=>{
+
+    }
+    const switchMode=()=>{
+        setisSignup((e)=>!e);
+        handleShowPassword(false);
+    }
+    const googleSuccess=()=>{
+        
+    }
+    const googleFailure=()=>{
 
     }
     return (
@@ -38,9 +50,25 @@ function Auth() {
                         <Input name="password" label="Password" handleChange={handleChange} type={showPassword?"text":"password"} handleShowPassword={handleShowPassword} />
                         {isSignup &&<Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password"  />}
                     </Grid>
+                <GoogleLogin
+                    clientId="Google ID"
+                    render={(renderProps)=>(
+                        <Button className={classes.googleButton} color='primary' fullWidth onClick={renderProps.onClick} disabled={renderProps.disabled} startIcon={<Icon/>} variant='contained' >Google SignIn</Button>
+                    )}
+                    onSuccess={googleSuccess}
+                    onFailure={googleFailure}
+                    cookiePolicy="single_host_origin"
+                />
                 <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                     {isSignup?'Sign Up':'Sign In'}
                 </Button>
+                <Grid container justify="flex-end">
+                    <Grid item>
+                        <Button onClick={switchMode}>
+                            {isSignup?'Already have an account? Sign In':'Don\'t have an account yet? Sign Up'}
+                        </Button>
+                    </Grid>
+                </Grid>
                 </form>
             </Paper>
         </Container>
