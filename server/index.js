ece6629379f77b10e079c43a423a53b93da0ca78
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-22 23:46:41 +0500

    back end

--- server/index.js
+++ server/index.js
@@ -1,28 +1,27 @@
 // added "type":"module", in package.json to use the react type syntax of importing files
 import express from 'express';
-import bodyparser from 'body-parser';
+import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js'; 
+import userRoutes from './routes/users.js'; 
+
 
 const app=express();
 dotenv.config();
 //Using express middleware to connect my app to the post routes
 
 //set up express and added body paret to limit the image size
-app.use(bodyparser.json({limit:"30mb",extended:true}));
-app.use(bodyparser.urlencoded({limit:"30mb",extended:true}));
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 app.use(cors());
 app.use('/posts',postRoutes);
-app.get('/',(req,res)=>{
-    res.send('Hello to memories');
-});
+app.use('/user',userRoutes);
 
 const PORT=process.env.PORT||5000;
 
-//connecting with mongoDB =>> useNewUrlParser:true,useUnifiedTopology:true are not required but we set them true to avoid errors in console
 //=>> .then run the app =>> .catch print the error
 mongoose.connect(process.env.CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
 .then(()=> app.listen(PORT,()=>console.log(`Server running on ${PORT}`)))
---
+++ server/routes/users.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+import {} from '../controllers/users.js'
+
+const router=express.Router();
+
+export default router;
