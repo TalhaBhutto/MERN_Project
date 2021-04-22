dabc86983bc29bc9a0cdc75880f82842bb439016
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-23 01:41:52 +0500

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -23,6 +23,7 @@ export const signup=async (req,res)=>{
         const existingUser= await User.findOne({email});
         if(existingUser) return res.status(400).json({message:"User already exists"});
         if(password!==confirmPassword) return res.status(400).json({message:"Passwards don't match."})
+        const hashedPassword=await bcrypt.hash()
 
     }
     catch(error){
