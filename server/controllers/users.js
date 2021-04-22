dce18a1030bb4954c705ed011e59ddbb3afedc66
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-23 01:12:12 +0500

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -7,7 +7,10 @@ export const signin=async (req,res)=>{
     const {email,password}=req.body;
     try{
         const existingUser=await User.findOne({email});
-        if(!existingUser) return res.status(404).json({message:"User doesn't exist"})
+        if(!existingUser) return res.status(404).json({message:"User doesn't exist"});
+        const isPasswordCorrect= await bcrypt.compare(password,existingUser.password);
+        if(!isPasswordCorrect) return res.status(400).json({message:"Invalid Credentials."});
+        const token=jwt.sign({email:existingUser.email,id:existingUser._id})
     }
     catch(e){
         console.log(e);
