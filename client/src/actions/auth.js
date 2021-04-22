6dad890c1a8215d66f80b97f01c6817e1c306596
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-22 23:38:51 +0500

    Update auth.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -4,9 +4,12 @@ import {AUTH} from '../constants/actionTypes';
 export const signin =(formData,history)=> async (dispatch) =>{
     try{}
     catch(error){
-        
+        console.log(error)
     }
 } 
 export const signup =()=> async (dispatch) =>{
-    
+    try{}
+    catch(error){
+        console.log(error)
+    }
 }
