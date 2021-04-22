1a5931ae261f94a9d3077a59243ccb8471437f42
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-23 01:08:21 +0500

    Update users.js

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -6,7 +6,8 @@ import User from '../models/user.js'
 export const signin=async (req,res)=>{
     const {email,password}=req.body;
     try{
-
+        const existingUser=await User.findOne({email});
+        if(!existingUser) return res.status(404).json({message:"User doesn't exist"})
     }
     catch(e){
         console.log(e);
