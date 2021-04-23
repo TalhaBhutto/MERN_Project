7c3cffcfb03c7ce6920d0e316fac2ff182428f91
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-23 20:02:35 +0500

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,7 +1,11 @@
 import jwt from 'jsonwebtoken';
 
 const auth= async (req,res,next)=>{
-    try{}
+    try{
+        const token=req.headers.authorization.split(" ")[1];
+        const isCustomAuth =token.lenght<500;
+        
+    }
     catch(error){
         console.log(error);
     }
