a5e321f6d083ffb97d0c7d84f06b5c9309cbe5ef
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-23 23:59:45 +0500

    a

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -37,8 +37,13 @@ export const deletePost=async (req,res)=>{
 }
 export const likePost=async (req,res)=>{
     const {id}=req.params;
+    if(!req.userId) return res.json({message: 'Unauthenticated'});
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send("No post with that id.");
     const post=await postMessage.findById(id);
+    const index=post.likes.findIndex((id)=>id===String(req.userId));
+    if(index===-1){
+        post.likes.push(req.userId);
+    }
     const updatedPost=await postMessage.findByIdAndUpdate(id,{likeCount:post.likeCount +1},{new:true});
     res.json(updatedPost);
 }
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ const auth= async (req,res,next)=>{
     try{
         const token=req.headers.authorization.split(" ")[1];
         const isCustomAuth =token.lenght<500;
-        const decodedData;
+        let decodedData;
         if(token && isCustomAuth){
             decodedData=jwt.verify(token,'test');
             req.userID=decodedData?.id;
