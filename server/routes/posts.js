fd629425359e4513c493cbf29026c97eeb563b15
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-26 00:59:21 +0500

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
+import { getPosts, createPost, updatePost, likePost, deletePost,searchPost } from '../controllers/posts.js';
 
 const router = express.Router();
 import auth from "../middleware/auth.js";
 
 router.get('/', getPosts);
+router.get('/:id',searchPost)
 router.post('/',auth,  createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
