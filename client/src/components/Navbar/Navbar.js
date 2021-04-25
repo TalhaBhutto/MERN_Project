08bfb396a501a09de55698b2687fe18a0ae339a4
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-26 00:23:10 +0500

    app bar 3

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -44,9 +44,9 @@ const Navbar = () => {
           matches => {
             return matches ? (
               <>
-                  <AppBar className={classes.appBar} position="static" color="inherit">
+                  <AppBar className={classes.appBar2} position="static" color="inherit">
                     <div className={classes.brandContainer}>
-                      <Typography component={Link} to="/" className={classes.heading} variant="h4" align="center">Memories</Typography>
+                      <Typography component={Link} to="/" className={classes.heading} variant="h3" align="center">Memories</Typography>
                       <img className={classes.image} src={memories} alt="icon" height="60" />
                     </div>
                   </AppBar>
@@ -56,19 +56,21 @@ const Navbar = () => {
                       <Button><SearchOutlinedIcon/></Button>
                       </div>
                       {
-                        !user?.result &&(
+                        !user?.result?(
                           <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                        ):
+                        (
+                          <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                         )
                       }
                     </AppBar>
                       {user?.result ? (
                         <AppBar className={classes.appBar} position="static" color="inherit">  
-                        <Toolbar className={classes.toolbar}>    
-                        <div className={classes.profile}>
+                        <Toolbar>    
+                        <div>
                           <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
                           <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
-                          <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
-                        </div>
+                          </div>
                         </Toolbar>
                         </AppBar>
                       ) : null}
--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -13,11 +13,12 @@ export default makeStyles((theme) => ({
   },
   appBar2: {
     borderRadius: 15,
+    margin: '30px 0',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    padding: '10px 50px',
+    padding: '10px 15px',
   },
   SearchBar: {
     display: 'flex',
