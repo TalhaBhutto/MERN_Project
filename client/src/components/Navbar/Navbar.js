a4c53cd74cbf78a4f34a40f8be5b397282a1c077
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-25 23:13:20 +0500

    Update Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -39,12 +39,39 @@ const Navbar = () => {
 
   return (
     <>
-      <Media query="(min-width:400px)">
+      <Media query="(max-width:400px)">
         {
           matches => {
             return matches ? (
-              <Typography component={Link} to="/" className={classes.heading} variant="h4" align="center">Memories</Typography>
-            )
+              <>
+                  <AppBar className={classes.appBar} position="static" color="inherit">
+                    <div className={classes.brandContainer}>
+                      <Typography component={Link} to="/" className={classes.heading} variant="h4" align="center">Memories</Typography>
+                      <img className={classes.image} src={memories} alt="icon" height="60" />
+                    </div>
+                  </AppBar>
+
+                  <AppBar className={classes.appBar} position="static" color="inherit">
+                    <div>
+                      <TextField></TextField>
+                      <Button  variant="contained" color="primary">Search</Button>
+                    </div>
+                  </AppBar>
+                    <AppBar className={classes.appBar} position="static" color="inherit">  
+                    <Toolbar className={classes.toolbar}>
+                      {user?.result ? (
+                        <div className={classes.profile}>
+                          <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+                          <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
+                          <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
+                        </div>
+                      ) : (
+                        <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+                      )}
+                    </Toolbar>
+                  </AppBar>
+                </>
+                )
               : (
                 <>
                   <AppBar className={classes.appBar} position="static" color="inherit">
