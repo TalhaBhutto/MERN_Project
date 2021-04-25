91412db4056048ed76a6301dd3a4519946a66342
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-25 23:30:29 +0500

    Update styles.js

--- client/src/components/Navbar/styles.js
+++ client/src/components/Navbar/styles.js
@@ -48,4 +48,7 @@ export default makeStyles((theme) => ({
     color: theme.palette.getContrastText(deepPurple[500]),
     backgroundColor: deepPurple[500],
   },
+  SearchText:{
+    maxWidth:'200px'
+  }
 }));
