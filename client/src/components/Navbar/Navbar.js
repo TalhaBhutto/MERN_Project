2b582fd37e582be9df8e11a4404fa9c5a186b60a
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-25 18:42:56 +0500

    added media query

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import { AppBar, Typography, Toolbar, Avatar, Button } from '@material-ui/core';
 import { Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import decode from 'jwt-decode';
+import media from 'react-media';
 
 import memories from '../../images/memories.png';
 import * as actionType from '../../constants/actionTypes';
