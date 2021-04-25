00048b94790c7ec4889f270236707b2077bdc93a
TalhaBhutto <74734659+TalhaBhutto@users.noreply.github.com>
2021-04-26 00:56:52 +0500

    Update index.js

--- client/src/Api/index.js
+++ client/src/Api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API=axios.create({baseURL:'https://memoriesbytalha.herokuapp.com'})
+const API=axios.create({baseURL:'https://localhost:5000'})
 API.interceptors.request.use((req)=>{
     if(localStorage.getItem('profile')){
         req.headers.Authorization=`Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
